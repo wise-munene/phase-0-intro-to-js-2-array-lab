e27173dbe320cce2b9321a47e2dbe29b4acb482e
Wise Munene Mutuku <wisemunene99@gmail.com>
2025-08-26 18:07:05 +0300

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution here!
+// Write your solution here!// 
+ const cats = ["Milo", "Otis", "Garfield"];
+ function destructivelyAppendCat(name){
+    cats.push("Ralph");
+ }
+ function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+ }
+ function destructivelyRemoveLastCat(name){
+    cats.pop("Garfield");
+ }
+ function destructivelyRemoveFirstCat(name){
+    cats.shift("Milo");
+ }
+ function appendCat(name){
+    return[...cats, "Broom"];
+ }
+ function prependCat(name){
+    return["Arnold",...cats];
+ }
+ function removeLastCat(){
+   return cats.slice(0, cats.length- 1 );
+ }
+ function removeFirstCat(){
+    return cats.slice(1);
+ }
